541e2ed107d80cc5bb223510306b01dea8a24859
Duncan Faulkner <info@e-ips.com>
2023-11-16 21:49:38 +0000

    fix(update): issues with linting - still using tslint needs upgrading to eslint

--- projects/apps/universal-demo-app/server.ts
+++ projects/apps/universal-demo-app/server.ts
@@ -1,4 +1,3 @@
-
 import 'zone.js/node';
 
 import { APP_BASE_HREF } from '@angular/common';
@@ -24,9 +23,12 @@ export function app(): express.Express {
   // Example Express Rest API endpoints
   // server.get('/api/**', (req, res) => { });
   // Serve static files from /browser
-  server.get('*.*', express.static(distFolder, {
-    maxAge: '1y'
-  }));
+  server.get(
+    '*.*',
+    express.static(distFolder, {
+      maxAge: '1y',
+    })
+  );
 
   // All regular routes use the Angular engine
   server.get('*', (req, res, next) => {
@@ -38,8 +40,7 @@ export function app(): express.Express {
         documentFilePath: indexHtml,
         url: `${protocol}://${headers.host}${originalUrl}`,
         publicPath: distFolder,
-        providers: [
-          { provide: APP_BASE_HREF, useValue: baseUrl },],
+        providers: [{ provide: APP_BASE_HREF, useValue: baseUrl }],
       })
       .then((html) => res.send(html))
       .catch((err) => next(err));
@@ -63,7 +64,7 @@ function run(): void {
 // The below code is to ensure that the server is run only when not requiring the bundle.
 declare const __non_webpack_require__: NodeRequire;
 const mainModule = __non_webpack_require__.main;
-const moduleFilename = mainModule && mainModule.filename || '';
+const moduleFilename = (mainModule && mainModule.filename) || '';
 if (moduleFilename === __filename || moduleFilename.includes('iisnode')) {
   run();
 }
--- tools/tslint-rules/noViewEncapsulationRule.ts
+++ tools/tslint-rules/noViewEncapsulationRule.ts
@@ -1,7 +1,7 @@
+import { minimatch } from 'minimatch';
 import path from 'path';
+import { IOptions, Rules, RuleWalker } from 'tslint';
 import ts from 'typescript';
-import {IOptions, Rules, RuleWalker} from 'tslint';
-import minimatch from 'minimatch';
 
 const ERROR_MESSAGE = 'Components must turn off view encapsulation.';
 
@@ -18,7 +18,6 @@ export class Rule extends Rules.AbstractRule {
 }
 
 class Walker extends RuleWalker {
-
   /** Whether the walker should check the current source file. */
   private _enabled: boolean;
 
@@ -32,28 +31,39 @@ class Walker extends RuleWalker {
     const relativeFilePath = path.relative(process.cwd(), sourceFile.fileName);
 
     // Whether the file should be checked at all.
-    this._enabled = fileGlobs.some(p => minimatch(relativeFilePath, p));
+    this._enabled = fileGlobs.some((p) => minimatch(relativeFilePath, p));
   }
 
   visitClassDeclaration(node: ts.ClassDeclaration) {
-    if (!this._enabled || !node.decorators) {
+    if (!this._enabled || !ts.canHaveDecorators(node)) {
       return;
     }
+    // if (!this._enabled || !node.decorators) {
+    //   return;
+    // }
 
-    node.decorators
-      .map(decorator => decorator.expression as any)
-      .filter(expression => expression.expression.getText() === 'Component')
-      .filter(expression => expression.arguments.length && expression.arguments[0].properties)
-      .forEach(expression => {
-        const hasTurnedOffEncapsulation = expression.arguments[0].properties.some((prop: any) => {
-          const value = prop.initializer.getText();
-          return prop.name.getText() === 'encapsulation' && value.endsWith('.None');
-        });
+    // node.decorators
+    //   .map((decorator) => decorator.expression as any)
+    //   .filter((expression) => expression.expression.getText() === 'Component')
+    //   .filter(
+    //     (expression) =>
+    //       expression.arguments.length && expression.arguments[0].properties
+    //   )
+    ts.getDecorators(node)
+      ?.map((decorator) => decorator.expression as any)
+      .filter((expression) => expression.expression.getText() === 'Component')
+      .forEach((expression) => {
+        const hasTurnedOffEncapsulation =
+          expression.arguments[0].properties.some((prop: any) => {
+            const value = prop.initializer.getText();
+            return (
+              prop.name.getText() === 'encapsulation' && value.endsWith('.None')
+            );
+          });
 
         if (!hasTurnedOffEncapsulation) {
           this.addFailureAtNode(expression.parent, ERROR_MESSAGE);
         }
       });
   }
-
 }
