d20d4dc934e9260ec004ba86b88609ee6eb7bc4f
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2025-02-25 13:28:12 +0100

    fix(universal-demo-app): correct application providers

--- projects/apps/universal-demo-app/src/main.server.ts
+++ projects/apps/universal-demo-app/src/main.server.ts
@@ -1,4 +1,12 @@
+import { APP_ID, importProvidersFrom } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
+import { FlexLayoutModule } from '@ngbracket/ngx-layout';
 import { AppComponent } from './app/app.component';
 
-export default () => bootstrapApplication(AppComponent);
+export default () =>
+  bootstrapApplication(AppComponent, {
+    providers: [
+      { provide: APP_ID, useValue: 'serverApp' },
+      importProvidersFrom(FlexLayoutModule.withConfig({ serverLoaded: true })),
+    ],
+  });
--- projects/apps/universal-demo-app/src/main.ts
+++ projects/apps/universal-demo-app/src/main.ts
@@ -1,15 +1,13 @@
-import { APP_ID } from '@angular/core';
+import { APP_ID, importProvidersFrom } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { FlexLayoutModule } from '@ngbracket/ngx-layout';
 import { AppComponent } from './app/app.component';
-import { SplitModule } from './app/split/split.module';
 
 document.addEventListener('DOMContentLoaded', () => {
   bootstrapApplication(AppComponent, {
     providers: [
       { provide: APP_ID, useValue: 'serverApp' },
-      FlexLayoutModule,
-      SplitModule,
+      importProvidersFrom(FlexLayoutModule.withConfig({})),
     ],
   }).catch((err) => console.error(err));
 });
