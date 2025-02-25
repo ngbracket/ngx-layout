f02d7ef490164bdd2ad504d07964b18cb719ae35
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2025-02-25 13:41:43 +0100

    refactor(demo-app): extract application routes

---
+++ projects/apps/demo-app/src/app/app.routes.ts
@@ -0,0 +1,29 @@
+import { Routes } from '@angular/router';
+
+export const DEMO_APP_ROUTES: Routes = [
+  { path: '', redirectTo: 'docs', pathMatch: 'full' },
+  {
+    path: 'grid',
+    loadChildren: () =>
+      import('./grid/grid.module').then((m) => m.DocsGridModule),
+  },
+  {
+    path: 'docs',
+    loadChildren: () =>
+      import('./layout/layout.module').then((m) => m.DocsLayoutModule),
+  },
+  {
+    path: 'responsive',
+    loadChildren: () =>
+      import('./responsive/responsive.module').then(
+        (m) => m.DocsResponsiveModule
+      ),
+  },
+  {
+    path: 'stackoverflow',
+    loadChildren: () =>
+      import('./stack-overflow/stack-overflow.module').then(
+        (m) => m.DocsStackOverflowModule
+      ),
+  },
+];
--- projects/apps/demo-app/src/app/routing.module.ts
+++ projects/apps/demo-app/src/app/routing.module.ts
@@ -1,34 +1,7 @@
 import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-
-const DEMO_APP_ROUTES: Routes = [
-  { path: '', redirectTo: 'docs', pathMatch: 'full' },
-  {
-    path: 'grid',
-    loadChildren: () =>
-      import('./grid/grid.module').then((m) => m.DocsGridModule),
-  },
-  {
-    path: 'docs',
-    loadChildren: () =>
-      import('./layout/layout.module').then((m) => m.DocsLayoutModule),
-  },
-  {
-    path: 'responsive',
-    loadChildren: () =>
-      import('./responsive/responsive.module').then(
-        (m) => m.DocsResponsiveModule
-      ),
-  },
-  {
-    path: 'stackoverflow',
-    loadChildren: () =>
-      import('./stack-overflow/stack-overflow.module').then(
-        (m) => m.DocsStackOverflowModule
-      ),
-  },
-];
+import { RouterModule } from '@angular/router';
+import { DEMO_APP_ROUTES } from './app.routes';
 
 @NgModule({
   exports: [RouterModule],
