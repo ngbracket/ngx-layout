afbfbfc1c62cf4aa9e89f11993e73448a33abb9d
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2025-02-25 13:31:49 +0100

    fix(universal-demo-app): add default application providers

--- projects/apps/universal-demo-app/src/main.server.ts
+++ projects/apps/universal-demo-app/src/main.server.ts
@@ -1,11 +1,13 @@
 import { APP_ID, importProvidersFrom } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
+import { provideServerRendering } from '@angular/platform-server';
 import { FlexLayoutModule } from '@ngbracket/ngx-layout';
 import { AppComponent } from './app/app.component';
 
 export default () =>
   bootstrapApplication(AppComponent, {
     providers: [
+      provideServerRendering(),
       { provide: APP_ID, useValue: 'serverApp' },
       importProvidersFrom(FlexLayoutModule.withConfig({ serverLoaded: true })),
     ],
--- projects/apps/universal-demo-app/src/main.ts
+++ projects/apps/universal-demo-app/src/main.ts
@@ -1,4 +1,4 @@
-import { APP_ID, importProvidersFrom } from '@angular/core';
+import { APP_ID, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { FlexLayoutModule } from '@ngbracket/ngx-layout';
 import { AppComponent } from './app/app.component';
@@ -6,6 +6,7 @@ import { AppComponent } from './app/app.component';
 document.addEventListener('DOMContentLoaded', () => {
   bootstrapApplication(AppComponent, {
     providers: [
+      provideZoneChangeDetection({ eventCoalescing: true }),
       { provide: APP_ID, useValue: 'serverApp' },
       importProvidersFrom(FlexLayoutModule.withConfig({})),
     ],
