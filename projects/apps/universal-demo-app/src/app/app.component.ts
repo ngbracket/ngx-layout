1c645ccf2cde338f50756953f5c2ea5de091eaaf
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2025-02-25 10:35:12 +0100

    fix(universal-demo-app): add missing Angular module imports

--- projects/apps/universal-demo-app/src/app/app.component.ts
+++ projects/apps/universal-demo-app/src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { FlexLayoutModule } from '@ngbracket/ngx-layout';
+import { SplitModule } from './split/split.module';
 
 @Component({
   selector: 'responsive-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
+  imports: [FlexLayoutModule, SplitModule],
 })
 export class AppComponent {
   title = 'responsive';
