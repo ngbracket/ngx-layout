c53ec47f0781d711acadd3c9a1ec276f8a03c82f
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2025-02-25 10:25:33 +0100

    refactor(universal-demo-app): remove redundant environment files

--- projects/apps/universal-demo-app/src/main.ts
+++ projects/apps/universal-demo-app/src/main.ts
@@ -1,13 +1,8 @@
-import { APP_ID, enableProdMode } from '@angular/core';
+import { APP_ID } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { FlexLayoutModule } from '@ngbracket/ngx-layout';
 import { AppComponent } from './app/app.component';
 import { SplitModule } from './app/split/split.module';
-import { environment } from './environments/environment';
-
-if (environment.production) {
-  enableProdMode();
-}
 
 document.addEventListener('DOMContentLoaded', () => {
   bootstrapApplication(AppComponent, {
