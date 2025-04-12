dacd703ec0ca15dd52c05581ea6138d8873d6132
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2025-04-13 01:00:35 +0200

    test(updated-demo): use Vitest for unit tests

---
+++ projects/apps/updated-demo/src/test-setup.ts
@@ -0,0 +1,13 @@
+import '@analogjs/vitest-angular/setup-zone';
+import './styles.scss';
+
+import { getTestBed } from '@angular/core/testing';
+import {
+  BrowserDynamicTestingModule,
+  platformBrowserDynamicTesting,
+} from '@angular/platform-browser-dynamic/testing';
+
+getTestBed().initTestEnvironment(
+  BrowserDynamicTestingModule,
+  platformBrowserDynamicTesting(),
+);
