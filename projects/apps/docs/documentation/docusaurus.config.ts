0d96af7aa2532c274073c85e38061c9d42042901
Duncan Faulkner <duncan@anglebrackets.dev>
2025-04-15 22:54:55 +0100

    feat(docs): add build script for documentation and update production URL

--- projects/apps/docs/documentation/docusaurus.config.ts
+++ projects/apps/docs/documentation/docusaurus.config.ts
@@ -10,7 +10,7 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://ngx-layout.ngbracket.com',
+  url: 'https://docs-ngx-layout.ngbracket.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
